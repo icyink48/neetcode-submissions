475c31d6f578a2ca2716ceedc5fe2343989f55a6
icyink48 <120611608+icyink48@users.noreply.github.com>
2026-09-11 20:57:34 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10 
+print(type(variable))
+variable = 10.1
+print(type(variable))
+variable = True
+print(type(variable))
+variable = 'varible'
+print(type(variable))
+variable = [0,1,2]
+print(type(variable))
